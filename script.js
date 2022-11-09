3938b12792f36bc13631c9b3e6516d3e19685ceb
Have Samuel <samiehave9@gmail.com>
2022-11-09 18:28:35 +0300

    Working on the navbar

---
+++ script.js
@@ -0,0 +1,13 @@
+const menuIcon = document.querySelector('.hamburger-menu');
+const navbar = document.querySelector('.bar');
+
+const list = document.querySelectorAll('.nav-list > li');
+list.forEach((element) => {
+  element.addEventListener('click', () => {
+    navbar.classList.toggle('change');
+  });
+});
+
+menuIcon.addEventListener('click', () => {
+  navbar.classList.toggle('change');
+});
