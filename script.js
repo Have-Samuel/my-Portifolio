2b97f7e473e925f18cd84f66e601f5db23ddebcd
Have Samuel <samiehave9@gmail.com>
2023-03-04 20:26:33 +0300

    bUilding Form Validation

--- script.js
+++ script.js
@@ -14,16 +14,20 @@ menuIcon.addEventListener('click', () => {
 });
 
 // Form validation
-const form = document.querySelector('form');
-const nameInput = document.querySelector('name["name-input"]');
-const emailInput = document.querySelector('name["email-input"]');
-const commentsInput = document.querySelector('name["comments-input"]');
+const $form = document.querySelector('.js-form');
+const $nameInput = document.querySelector('.js-name-input');
+const $emailInput = document.querySelector('.js-email-input');
+const $comsInput = document.querySelector('.js-textarea');
 
-const validate() = () => {
-  
-}
+const validate = () => {
+  const nameValue = $nameInput.value.trim();
+  const emailValue = $emailInput.value.trim();
+  const comsValue = $comsInput.value.trim();
 
-form.addEventListener('submit', (e) => {
+  console.log('validate', nameValue, emailValue, comsValue);
+};
+
+$form.addEventListener('submit', (e) => {
   e.preventDefault();
 
   validate();
