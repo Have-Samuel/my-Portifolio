2acc3a163b2a559835795eda2a1ea9dad21be48c
Have Samuel <samiehave9@gmail.com>
2023-04-04 06:17:00 +0300

    Working on the cards

--- script.js
+++ script.js
@@ -34,25 +34,23 @@ $form.addEventListener('submit', (e) => {
 });
 
 // PROJECTS
-const allProjects = document.querySelector('.project');
+const allProjects = document.querySelector('#projects');
 // const popup = document.querySelector('.popup-project');
 
-const allProjects = [
+const Project = [
   {
     projectId: 'projectOne',
+    featuredImage: './images/Background.png',
     name: 'Printing Data One',
     description: 'A daily selection of privately personalized reads; no accounts or ',
-    featuredImage: './images/Background.png',
     technologies: ['html', 'Bootstrap', 'Ruby'],
-    liveLink: 'https://have-samuel.github.io/portfolio1/',
-    sourceLink: 'https://github.com/Have-Samuel/portfolio1/tree/popup-menu',
   },
 ];
 
-const arrClassName = ['project1', 'project2', 'project3', 'project4', 'project5', 'project6'];
+// const arrClassName = ['project1', 'project2', 'project3', 'project4', 'project5', 'project6'];
 
 
-const projects = document.querySelector('.project');
+const projects = document.querySelector('sproject');
 
 
 
