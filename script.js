f407121bebd93b576d9eddf0f036b77ab14fbb82
Have Samuel <samiehave9@gmail.com>
2023-04-09 21:32:24 +0300

    Remove unnecessery comments

--- script.js
+++ script.js
@@ -13,26 +13,6 @@ menuIcon.addEventListener('click', () => {
   navbar.classList.toggle('change');
 });
 
-// Form validation
-// const $form = document.querySelector('.js-form');
-// const $nameInput = document.querySelector('.js-name-input');
-// const $emailInput = document.querySelector('.js-email-input');
-// const $comsInput = document.querySelector('.js-textarea');
-
-// const validate = () => {
-//   const nameValue = $nameInput.value.trim();
-//   const emailValue = $emailInput.value.trim();
-//   const comsValue = $comsInput.value.trim();
-
-//   console.log('validate', nameValue, emailValue, comsValue);
-// };
-
-// $form.addEventListener('submit', (e) => {
-//   e.preventDefault();
-
-//   validate();
-// });
-
 // PROJECTS
 const projects = [
   {
