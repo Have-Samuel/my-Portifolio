d31d093db7520e6eaca45876a205ffdaf9fc62f1
Have Samuel <samiehave9@gmail.com>
2023-04-24 00:33:07 +0300

    Displaiyinh cards

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ menuIcon.addEventListener('click', () => {
 // PROJECTS
 const projects = [
   {
-    title: 'Printing One Data',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry\'s standard.',
+    title: 'Portfolio Website',
+    description: 'Basic Portfolio Website that built with HTML, CSS and JavaScript, and shows two pages as a sample if more needed.',
     popupDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.',
     featuredImage: './desktop-images/imagecard4.png',
     popupImg: './images/popup.png',
